86bdb29be69ef310a3ce7a6ada63a744cd5006b1
Augusto Saboia <augustosaboiaestudos@gmail.com>
2024-08-01 23:22:54 -0300

    Update vetor_exercicio1.c

--- Estrutura de Dados II/Unidade 01/Atividades/vetor_exercicio1.c
+++ Estrutura de Dados II/Unidade 01/Atividades/vetor_exercicio1.c
@@ -1,32 +1,32 @@
-/* Escreva um algoritmo que solicite a quantidade de avaliações no semestres(entre uma e dez provas) notas de um aluno e ao final apresente se aprovado ou não conforme a média aritmética das N avaliações
-1) Ao final apresenter a soma das notas e a média aritmética delas
-2) Apresentar qual a avaliação que teve a maior nota(exemplo: A segunda prova teve a maior nota).
-3) Apresentar qual a avaliação que teve a menor nota (exemplo: A primeira prova teve a menor nota).
-4) Solicitar ao usuário no ínicio qual a média na faculdade para aprovação e informar se aprovado ou não. */
+/* Escreva um algoritmo que solicite a quantidade de avaliaÃ§Ãµes no semestres(entre uma e dez provas) notas de um aluno e ao final apresente se aprovado ou nÃ£o conforme a mÃ©dia aritmÃ©tica das N avaliaÃ§Ãµes
+1) Ao final apresenter a soma das notas e a mÃ©dia aritmÃ©tica delas
+2) Apresentar qual a avaliaÃ§Ã£o que teve a maior nota(exemplo: A segunda prova teve a maior nota).
+3) Apresentar qual a avaliaÃ§Ã£o que teve a menor nota (exemplo: A primeira prova teve a menor nota).
+4) Solicitar ao usuÃ¡rio no Ã­nicio qual a mÃ©dia na faculdade para aprovaÃ§Ã£o e informar se aprovado ou nÃ£o. */
 #include <locale.h>
 #include <stdio.h>
 #include <conio.h>
 int main(){
-    setlocale(LC_ALL, "Portuguese"); // Console em Português
+    setlocale(LC_ALL, "Portuguese"); // Console em PortuguÃªs
 
     float nota[10], mediaFaculdade, maiorNota[1], menorNota[1];
     int i, qtdProvas;
     nota[0] = 0;
-    // Atribuindo valor a variável mediaFaculdade
-    printf("Informe a média de aprovação: ");
+    // Atribuindo valor a variÃ¡vel mediaFaculdade
+    printf("Informe a mÃ©dia de aprovaÃ§Ã£o: ");
     scanf("%f", &mediaFaculdade);
-    // Repetindo a variável qtdProvas até que as solicitações sejam atendidas
+    // Repetindo a variÃ¡vel qtdProvas atÃ© que as solicitaÃ§Ãµes sejam atendidas
     do{
     printf("\n Informe a quantidade de provas: ");
     scanf("%i", &qtdProvas); 
     if (qtdProvas < 1 || qtdProvas >10)
-        printf("\n Atenção!!! Entre UM e DEZ!!!"); }
+        printf("\n AtenÃ§Ã£o!!! Entre UM e DEZ!!!"); }
     while (qtdProvas < 1 || qtdProvas >10);
-    // Atribuindo valor ás qtdProvas usando o vetor nota[10]
+    // Atribuindo valor Ã¡s qtdProvas usando o vetor nota[10]
     for (i=1; i<=qtdProvas; i++){
-        printf(" Informe a %iº nota do aluno: ", i);
+        printf(" Informe a %iÂº nota do aluno: ", i);
         scanf("%f", &nota[i]);
-        nota[0] += nota[i]; // A primeira posição do vetor está acumulando a soma das notas
+        nota[0] += nota[i]; // A primeira posiÃ§Ã£o do vetor estÃ¡ acumulando a soma das notas
         if (i == 1){
         	maiorNota[0] = nota[i];
         	maiorNota[1] = i;
@@ -44,22 +44,22 @@ int main(){
 				}
 			}
     }
-	// Mostrando ao aluno se foi aprovado ou não baseado nas notas alcançadas e a média da faculdade
+	// Mostrando ao aluno se foi aprovado ou nÃ£o baseado nas notas alcanÃ§adas e a mÃ©dia da faculdade
     if(nota[0] / qtdProvas >= mediaFaculdade){
-        printf("\n Parabéns!!! Você foi aprovado com média %.2f.", nota[0] / qtdProvas);
+        printf("\n ParabÃ©ns!!! VocÃª foi aprovado com mÃ©dia %.2f.", nota[0] / qtdProvas);
     }
     else{
-        printf("\n Infelizmente você não foi aprovado. Sua média foi de %.2f e você precisava de %.2f.",nota[0] / qtdProvas, mediaFaculdade);
+        printf("\n Infelizmente vocÃª nÃ£o foi aprovado. Sua mÃ©dia foi de %.2f e vocÃª precisava de %.2f.",nota[0] / qtdProvas, mediaFaculdade);
     }
     
-	// 2) Apresentar qual a avaliação que teve a maior nota(exemplo: A segunda prova teve a maior nota).
-	printf("\n Relatório de provas");
+	// 2) Apresentar qual a avaliaÃ§Ã£o que teve a maior nota(exemplo: A segunda prova teve a maior nota).
+	printf("\n RelatÃ³rio de provas");
 	
 	
-    // 1) Ao final apresenter a soma das notas e a média aritmética delas
-    printf("\n A %.0fº prova teve a maior nota. %.2f pontos", maiorNota[1], maiorNota[0]);
-	printf("\n A %.0fº prova teve a menor nota. %.2f pontos", menorNota[1], menorNota[0]);
+    // 1) Ao final apresenter a soma das notas e a mÃ©dia aritmÃ©tica delas
+    printf("\n A %.0fÂº prova teve a maior nota. %.2f pontos", maiorNota[1], maiorNota[0]);
+	printf("\n A %.0fÂº prova teve a menor nota. %.2f pontos", menorNota[1], menorNota[0]);
     printf("\n A soma das notas %.2f.", nota[0]);
-    printf("\n A média das notas %.2f.", nota[0] / qtdProvas);
+    printf("\n A mÃ©dia das notas %.2f.", nota[0] / qtdProvas);
 
 }
