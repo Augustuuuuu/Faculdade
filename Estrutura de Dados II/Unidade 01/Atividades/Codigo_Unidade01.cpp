b2e3c10c86d386b52fd89258eee3044aa4155ff7
Augusto Saboia <augustosaboiaestudos@gmail.com>
2024-08-16 08:49:48 -0300

    .

--- Estrutura de Dados II/Unidade 01/Atividades/Codigo_Unidade01.cpp
+++ Estrutura de Dados II/Unidade 01/Atividades/Codigo_Unidade01.cpp
@@ -1,29 +1,24 @@
 #include <stdio.h>
 #include <locale.h>
-// Escreva um algortimo que solicite ao usuï¿½rio N valores nï¿½mericos e ao final apresente:
-// Versï¿½o01: O maior e o menor
-// Versï¿½o02: As medidas de tendencia central Media, Moda e Mediana.
-// Versï¿½o03: Implementar Nome, Idade, Sexo e Trï¿½s notas dos alunos
+// Escreva um algortimo que solicite ao usuário N valores númericos e ao final apresente:
+// Versão01: O maior e o menor
+// Versão02: As medidas de tendencia central Media, Moda e Mediana.
+// Versão03: Implementar Nome, Idade, Sexo e Três notas dos alunos
 
 int main(){
 	
-	setlocale(LC_ALL, "Portuguese"); // Console em Portuguï¿½s
+	setlocale(LC_ALL, "Portuguese"); // Console em Português
 	    
-    float Valor, Maior, Menor, Soma, Moda, Mediana, valores[100];
-    int Qtd = 0, resposta, repeticao[100] = {0}, maior_repeticao = 0, i = 0;
-    
+    float Valor, Maior, Menor, Soma, Moda, Mediana;
+    int Qtd = 0, resposta, freq[100] = {0}, maior_freq = 0;
     do{
         Qtd++;
         // Entrada de dados
-        printf("\n Informe o %i.ï¿½ valor: ", Qtd);
+        printf("\n Informe o %i.º valor: ", Qtd);
         scanf("%f", &Valor);
-        Soma += Valor;
-        int valor_convertido = (int)Valor;
-        repeticao[valor_convertido]++;
+        int valorConvertido = int(Valor);
+        freq[valorConvertido]++;
         // Processamento de dados
-        valores[i++] = valor_convertido;
-        
-        
         if(Qtd ==1){
         	Maior = Valor;
         	Menor = Valor;
@@ -33,29 +28,25 @@ int main(){
             Maior = Valor;
         else if(Valor < Menor)
             Menor = Valor;
-                
-        
-
-        printf("\n Deseja informar o prï¿½ximo valor? 1/Sim - 2/Nï¿½o ");
+            
+            
+            
+            
+        printf("\n Deseja informar o próximo valor? 1/Sim - 2/Não ");
         scanf("%i", &resposta);
     } while(resposta == 1);
     for (int i = 0; i < Qtd; i++){
-        if (repeticao[i] > maior_repeticao){
-            maior_repeticao = repeticao[i];
+        if (freq[i] > maior_freq){
+            maior_freq = freq[i];
             Moda = i;
         }
-    int meio = Qtd / 2;
-    Mediana = (Qtd % 2 == 0) ? (valores[meio - 1] + valores[meio]) / 2.0 : valores[meio];
     }
-    // Criar um vetor para verificar quais numeros se repetem enquanto o usuï¿½rio informa
-    // Saï¿½da de dados
+    // Criar um vetor para verificar quais numeros se repetem enquanto o usuário informa
+    // Saída de dados
     printf("\n O maior: %0.2f", Maior);
     printf("\n O menor: %0.2f", Menor);
-    printf("\n Mï¿½dia: %0.2f", Soma / Qtd);
+    printf("\n Média:  %0.2f", Soma / Qtd);
     printf("\n Moda: %0.2f", Moda);
     printf("\n Mediana: %0.2f", Mediana);
     
 } 
-
-
-
